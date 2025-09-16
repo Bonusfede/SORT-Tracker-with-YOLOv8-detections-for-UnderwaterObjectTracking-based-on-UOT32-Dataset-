d9de895c536043e32fdd0b7eab2e287069a8771e
Bonusfede <67276466+Bonusfede@users.noreply.github.com>
2025-09-16 11:34:52 +0200

    Aggiornamento per avere tutti i file pickle

--- detections.py
+++ detections.py
@@ -1,14 +1,30 @@
+import os
 from ultralytics import YOLO
-from utils import * 
-import torch
+from utils import conv_video_in_frames, detect_get_pandas_df, save_df_to_pickle
 
-# Caricamento del rilevatore (YOLOv8 in questo caso)
-model = YOLO('best.pt')     # modello già allenato 
+video = "C:/Users/fedem/Desktop/Progetto/archive"  # cartelle con i video
+df_out = "df_pickles"
+os.makedirs(df_out, exist_ok=True)
 
-# Conversione del video in una lista di frame
-images, fpsses = conv_video_in_frames('video_in/ArmyDiver1.mp4')
+model = YOLO('best.pt')
 
-# Predizioni e salvataggio del dataframe sul video
-df_list = detect_get_pandas_df(model, images)
+for folder_name in os.listdir(video):
+    folder_path = os.path.join(video, folder_name)
+    if not os.path.isdir(folder_path):
+        continue
 
-save_df_to_pickle(df_list, 'df_list_army.pkl')  
+    #Trova il file video nella cartella
+    video_file = None
+    for f in os.listdir(folder_path):
+        if f.endswith((".mp4", ".avi", ".mov")):
+            video_file = os.path.join(folder_path, f)
+            break
+
+ 
+    if video_file is None:
+        continue
+
+    df_list = detect_get_pandas_df(model, video_file)
+    pickle_path = os.path.join(df_out, f"df_{folder_name}.pkl")
+    save_df_to_pickle(df_list, pickle_path)
+    print(f"Salvato pickle: {pickle_path}")
